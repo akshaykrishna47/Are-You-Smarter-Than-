4ab71031d6d0ddbf48ae42e7c32c329e2b5bf2c9
Akshay <akshaykrishna00a@gmail.com>
2026-06-18 13:07:42 +0800

    initial commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,11 @@ function Home() {
       <div className="home-stage">
         <img src={logo} alt="Game logo" className="home-logo" />
         <Link to="/Menu">
-          <button type="button" className="home-btn">▶</button>
+          <button type="button" className="home-btn">
+            <svg viewBox="0 0 24 24" width="22" height="22" aria-hidden="true">
+              <path class="pp-morph-icon" d="M8 5 L8 19 L19 12 Z" fill="currentColor" />
+            </svg>
+        </button>
         </Link>
       </div>
     </div>
